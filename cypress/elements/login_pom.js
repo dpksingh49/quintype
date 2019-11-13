db802b150f96b64afccde073007423cae187f661
Deepak Kumar <DK02184133@softenger.local>
2019-11-13 11:39:48 +0530

    first commit

---
+++ cypress/elements/login_pom.js
@@ -0,0 +1,59 @@
+export class Login{
+    loginIcon = '[data-test-id="user-profile"]';
+    name = "input[type = 'text']";
+    email = 'input[type="email"]';
+    emailInvalidMessage = '[data-testid="email-invalid"]';
+    pass = 'input[type="password"]';
+    passInvalidMessage = '[data-testid="password-invalid"]';
+    wrongCredentials = '[data-testid="wrong-credentials"]';
+    loginButton = 'button[data-testid=signin-submit]';
+    subscribeButton = '[data-test-id="subscribe"]';
+    MyProfile = "user-profile-m__white__2AI0Z";
+    subscribePlan = '[data-testid="plans-subscribe-btn"]';
+    phoneNumber = "#contact";
+    menu_India =".header-three-m__default-menu__24xMV > ul > li:nth-child(1) > a";
+    menu_SubscriptionStory =".header-three-m__default-menu__24xMV > ul > li:nth-child(2) > a";
+    menu_Politics =".header-three-m__default-menu__24xMV > ul > li:nth-child(3) > a";
+    menu_Technology =".header-three-m__default-menu__24xMV > ul > li:nth-child(4) > a";
+    menu_Culture =".header-three-m__default-menu__24xMV > ul > li:nth-child(5) > a";
+    submenu_Health = ".header-three-m__default-menu__24xMV > ul > li:nth-child(1) > ul > li:nth-child(1) > a";
+    submenu_Technology = ".header-three-m__default-menu__24xMV > ul > li:nth-child(1) > ul > li:nth-child(2) > a";
+    searchIcon = "[data-test-id=search-icon]";
+    searchTypeArea = "#searchForm"
+
+    verifyingLogin(element) {
+      cy.get(element).click({ force: true });
+
+    }
+    verifyingElement(element, text) {
+      cy.get(element)
+        .contains(text)
+        .click({ force: true });
+    }
+    verifyingInvalidCredentialsMessage(element, text) {
+      cy.get(element).contains(text);
+    }
+  
+    verifyingElementVisibility(element, text) {
+      cy.get(element).type(text);
+    }
+    verifyingSubscription(element, text) {
+      cy.get(element)
+        .contains(text)
+        .click({ force: true });
+    }
+    verifyingFormField(element, text) {
+      cy.get(element)
+        .clear()
+        .type(text);
+    }
+    verifyingElementEnabled(element, text) {
+      cy.get(element).contains(text);
+    }
+    verifyingSearchBtn(element) {
+      cy.get(element).click({ force: true });
+    }
+    verifyingSearchForm(element,text){
+      cy.get(element).type(text);
+    }
+}
---
+++ cypress/integration/VerifyLoginPage.spec.js
@@ -0,0 +1,49 @@
+///  <reference  types="cypress" />
+
+import {Login} from "../elements/login_pom";  
+
+const login = new Login();
+describe("Verifying Login button on homepage", function() {
+    beforeEach(function() {
+      // Cookies are cleared upon each test. Please preserve cookies for further actions..
+      Cypress.Cookies.preserveOnce("session-cookie", "thinmint");
+    });
+  
+    it("Verifying Login", function() {
+      cy.visit("/");
+      login.verifyingLogin(login.loginIcon);
+      cy.wait(3000);
+    });
+  
+    it("Verifying login with wrong credentials", function() {
+      login.verifyingElementVisibility(login.email, "calacenu@royalhost.in");
+      login.verifyingElementVisibility(login.pass, "000000");
+      login.verifyingElement(login.loginButton, "Sign in");
+      login.verifyingInvalidCredentialsMessage(login.wrongCredentials, "Wrong credentials");
+    });
+  
+    it("Verifying login form ", function() {
+      login.verifyingFormField(login.email, "calacenu@royalhost.info");
+      login.verifyingFormField(login.pass, "0000");
+      login.verifyingElement(login.loginButton, "Sign in");
+      cy.wait(3000);
+    });
+    it("Verifying menu items are loaded", function(){
+      login.verifyingElementEnabled(login.menu_India, "India");
+      login.verifyingElementEnabled(login.submenu_Health, "Health")
+      login.verifyingElementEnabled(login.submenu_Technology, "Technology")
+      login.verifyingElementEnabled(login.menu_SubscriptionStory, "Subscription Story");
+      login.verifyingElementEnabled(login.menu_Politics, "Politics ");
+      login.verifyingElementEnabled(login.menu_Technology, "Technology");
+      login.verifyingElementEnabled(login.menu_Culture, "Culture");
+    });
+
+    it("Verifying Search button", function(){
+      login.verifyingSearchBtn(login.searchIcon)
+    })
+
+    //it("verifying SearchType area", function(){
+    //  login.verifyingSearchForm(login.searchTypeArea,"India");
+   // })
+  });
+
